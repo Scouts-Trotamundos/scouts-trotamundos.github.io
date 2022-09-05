2d9a2531cf445555130766aff8c2479473a33a35
Simdrom <ksimdrom@hotmail.es>
2022-09-05 11:16:59 +0200

    Start to migrate HTML to REACT code in scouters form

--- src/components/ContactForm/ContactParts/ContactPartsText.js
+++ src/components/ContactForm/ContactParts/ContactPartsText.js
@@ -1,11 +1,13 @@
 import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 import { ValidationError } from "@formspree/react";
 
 const ContactPartsText = (props) => {
   return (
     <Row>
-      <label htmlFor={props.name}>
+      {/* <label htmlFor={props.name}>
         {props.label}
 
         {!props.isTextArea && (
@@ -30,7 +32,34 @@ const ContactPartsText = (props) => {
             },
           ]}
         />
-      </label>
+      </label> */}
+
+      <Form>
+        {!props.isTextArea && (
+          <>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>{props.name}</Form.Label>
+              <Form.Control type={props.name} placeholder={props.label} />
+              <Form.Text className="text-muted">
+                El <b>{props.name}</b> {props.validationErrorMessage}
+              </Form.Text>
+            </Form.Group>
+          </>
+        )}
+        {props.isTextArea && (
+          <>
+            <Form.Label htmlFor={props.name}>{props.name}</Form.Label>
+            <Form.Control
+              type="password"
+              id={props.name}
+              aria-describedby={props.name}
+            />
+            <Form.Text id={props.name} muted>
+              El <b>{props.name}</b> {props.validationErrorMessage}
+            </Form.Text>
+          </>
+        )}
+      </Form>
     </Row>
   );
 };
